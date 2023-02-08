e262310ae26925b19c0d719a8aabc133c208366f
Zhang Zhanpeng <zhangregister@outlook.com>
2023-02-08 16:37:52 +0800

    debug ok

---
+++ config.js
@@ -0,0 +1,16 @@
+const Title = '示范应用'
+
+const Demo = {
+    pubKey: `-----BEGIN -----
+MIIBCgKCAQEAnE8MYWxiyOnL+g4NPF4ZJH/BFJ48OOHBPiHXBGl5R5BXKht3F6ng
+OgQT7cgxhz6EhaMRD/b8o4ef897p/lHViMeYkJjbDN9/qeBNiwIflIS5jqlzr9j4
+wKQWSx6Da3LaiGEAsq1SAJTRvXgyYhxBWW8n553NGjT71vf2wfaxMiDNAPwBoBq7
++GToKXyErnmhcpbWrqAnl4Fevk4mMBCguZ2PbMQIRooNg9TYdQbBq6QI54fNtMZp
+i3V/y3HnpTJpzvFpjX419Du/pCK+1KhzJPrzC24ATO/bmq9pb5avD1ixEBMNjkAI
+86z8TxJ3kgLnONj89wEJG7+OtoYHuzGhUwIDAQAB
+-----END -----`
+
+
+}
+
+const baseURL = '/'
